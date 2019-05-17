f300fb0c5edc021bfcacb07f5cf86ff3faed7a95
Nick O <nicholas.oetinger@gmail.com>
2019-05-17 15:11:16 -0400

    initial commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,52 @@
+//Variables:
+var questions = [{
+    question: "?" ,
+    choices: [?,?,?,?] ,
+    answer: ? , //Answer is the index of choices
+}
+{
+    question: "?" ,
+    choices: [?,?,?,?] ,
+    answer: ? , //Answer is the index of choices
+}
+{
+    question: "?" ,
+    choices: [?,?,?,?] ,
+    answer: ? , //Answer is the index of choices
+}
+{   question: "?" ,
+    choices: [?,?,?,?] ,
+    answer: ? , //Answer is the index of choices
+}
+{
+    question: "?" ,
+    choices: [?,?,?,?] ,
+    answer: ? , //Answer is the index of choices
+}];
+
+var userChoice = []; //User selection
+var questionCounter = 0; //Question #
+var correctAnswers = 0; //# of correct answers
+var incorrectAnswers = 0; //# of incorrect answers
+var unansweredQuestions = 0; //# of unanswered questions
+
+
+
+//Start Game Function
+
+//Timer Function
+
+//Question Function
+
+//Click Function
+    //Check/Compare Answer Function
+
+    //Next Question Function
+
+//# of Correct Answers
+
+//# of Incorrect Answers
+
+//# of Unanswered
+
+//Reset Game Function
